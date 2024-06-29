ce7d59fbe75847424c42ab446672e9189ea93140
kachi33 <72194813+kachi33@users.noreply.github.com>
2024-06-29 22:11:45 +0100

    feat: Implement useState for dropdown visibility
    Implement responsive header with dropdown menu
    
    - Added useState to manage visibility of navigation and dropdown menu.
    - Implemented toggleVisibility function to handle state changes for navVisible and dropdownVisible.
    - Adjusted logo size based on viewport.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/header.js';
 
+
 const App = function() {
   return (
     <Header/>
--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../header.css';
-import logo from '../images/logo.png';
+import logo from '../images/logo.png';import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'; // Import icons
+
+
 
 const Header = function() {
+    const [isVisible, setIsVisible] = useState({
+        navVisibilty: false,
+        dropdownVisible: false
+    });
+
+    const toggleVisibility = (key) => {
+        setIsVisible((prevState) => ({
+            ...prevState, 
+            [key]: !prevState[key],
+        }));
+      };
+
+    const handleDropdownClick = () => {
+        toggleVisibility('navVisible');
+        toggleVisibility('dropdownVisible');
+    };
+    
     return( 
         <header className='header'>
             <div className='header-logo'>
-                <img alt='logo' src={logo} style={{ height: '60px', width: 'auto' }}/>
-                <h1 className='header-tagline'>Digital Lab Assistant</h1>
+                <img alt='logo' src={logo} />
+                <h1 className= 'header-tagline'>Digital Lab Assistant</h1>
             </div>
-            <nav className='header-nav'>
+            <nav className={`header-nav ${isVisible.navVisibilty ? 'visible' : ''}`}>
                 <ul>
                 <li><a href='#'>Home</a></li>
                 <li><a href='#'>Contact Us</a></li>
                 </ul>
             </nav>
+            <div className="header-collapse header-dropdown" onClick={handleDropdownClick} >
+                    <FontAwesomeIcon icon={faBars} size='2x' style={{ color: "#283618"}} />
+                    {isVisible.dropdownVisible && (
+                        <ul className="dropdown-menu">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                     )}
+            </div>
         </header>
     );
 }
