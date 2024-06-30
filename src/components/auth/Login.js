0a9527314da53dcaabf35cbc13cb2119d0a9e666
kachi33 <72194813+kachi33@users.noreply.github.com>
2024-06-30 20:47:52 +0100

    chore-Refined login form appearance
    Enhanced UI with customized input fields and gradient background.

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,22 +1,125 @@
-import Logo from "../UI/Logo";
-import LoginForm from "./LoginForm";
+import * as React from 'react';
+// import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Logo from '../UI/Logo';
+import exhibitImage from '../../images/exhibit01.jpg';
 
-const Login = function(){
-    return( 
-        <div className="login">
-        <div className="login-blk">
-            <Logo />
-            <h2>Log into your account</h2>
-            <LoginForm/>
-        </div>
-        <div className="login-exihbit">
-        </div>
+// function Copyright(props) {
+//   return (
+//     <Typography variant="body2" color="text.secondary" align="center" {...props}>
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://mui.com/">
+//         Your Website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
 
-     </div>
+// TODO remove, this demo shouldn't need to reset the theme.
 
-        // login form 
-        // design 
-        )
+const defaultTheme = createTheme();
+
+export default function SignInSide() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `linear-gradient(to right, #283618, #fefae0), url(${exhibitImage})`,
+            // backgroundColor: (t) =>
+            //   t.palette.mode === 'light' ? 'rgba(255, 255, 255, 0.5)' : 'rgba(0, 0, 0, 0.5)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundBlendMode: 'overlay'
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'left',
+            }}
+          >
+            <Logo/>
+            <Typography component="h2" variant="h5" sx={{mt: 6}}   >
+                {/*  mx: 8, my: 4*/}
+              Log in to your account
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{
+                    mt: 3,
+                    mb: 2,
+                    backgroundColor: '#606c38', 
+                    color: '#ffffff', 
+                    '&:hover': {
+                      backgroundColor: '#283618', 
+                    },
+                  }}   
+                >
+                Sign In
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+     </ThemeProvider>
+  );
 }
-
-export default Login;
